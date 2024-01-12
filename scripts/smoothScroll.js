37a8ee32d5d64661939d021bdbc88700bb233efd
sici1y <153026747+sici1y@users.noreply.github.com>
2024-01-12 20:47:26 +0000

    scroll to top

--- scripts/smoothScroll.js
+++ scripts/smoothScroll.js
@@ -8,3 +8,8 @@ $("#scroll-2").on("click", function() {
         scrollTop: $("#scrollShortcutTwo").offset().top
      });
 });
+$("#scroll-3").on("click", function() {
+    $('html,body').animate({
+        scrollTop: $("#scrollToTop").offset().top
+     });
+});
